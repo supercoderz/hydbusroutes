90a870f06f6f6b7e7d970536f8c6bdaccf6953ff
Narahari Allamraju <anarahari@gmail.com>
2014-03-08 20:06:13 +0800

    fixed issue with routes by adding paths instead of stars

--- routeplanner/simple_planner.py
+++ routeplanner/simple_planner.py
@@ -12,19 +12,25 @@ for item in data:
 	stops = item['stops']
 	stop_names = [stop['stop']['name'] for stop in stops]
 	if stop_names!=[]:
-		graph.add_star(stop_names,route=route_name)
+		#graph.add_star(stop_names,route=route_name)
 		graph.add_path(stop_names,route=route_name)
-		stop_names.reverse()
-		graph.add_star(stop_names,route=route_name)
+		#stop_names.reverse()
+		#graph.add_path(stop_names,route=route_name)
+		#graph.add_star(stop_names,route=route_name)
 
 #print nx.shortest_path(graph,'Dilsukhnagar Bus station','Patancheru Bus Stop')
 #print nx.shortest_path(graph,'Patancheru Bus Stop','Hayath Nagar Bus Stop')
 #for path in nx.all_shortest_paths(graph,'Dilsukhnagar Bus station','Patancheru Bus Stop'):
 #	print path
 	
-all_paths =  nx.all_pairs_shortest_path(graph)
-path =  all_paths['Chaitanyapuri']['Kothapet Bus Stop']
-for k,v in zip(path,path[1:]):
-	edges = graph[k][v]
-	for edge in edges.values():
-		print k,v,edge['route']
+#all_paths =  nx.all_pairs_shortest_path(graph)
+#path = all_paths['Chaitanyapuri']['Ziaguda']
+paths = nx.all_shortest_paths(graph,'Chaitanyapuri','Ziaguda')
+for path in paths:
+	for k,v in zip(path,path[1:]):
+		edges = graph[k][v]
+		routes = []
+		for edge in edges.values():
+			routes.append(edge['route'])
+		#print k,v,routes
+	print path
