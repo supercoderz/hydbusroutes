5237f253f3e412a156e48031ac1d2bea4cd266a3
Narahari Allamraju <anarahari@gmail.com>
2014-03-09 11:42:27 +0800

    added simple tornado based path server to return results from graph over http

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 hydbusroutes
 ============
 
-A simple project to get all the bus routes in Hyderabad and demonstrate how we can build a rudimentary route planner
+A simple project to get all the bus routes in Hyderabad and demonstrate how we can build a rudimentary route planner.
---
+++ routeplanner/path_server.py
@@ -0,0 +1,63 @@
+import json
+import networkx as nx
+from itertools import izip
+import tornado.ioloop
+import tornado.web
+import os
+
+dirname = os.path.dirname(__file__)
+STATIC_PATH = os.path.join(dirname, 'static')
+TEMPLATE_PATH = os.path.join(dirname, 'templates')
+graph_builder = None
+
+class GraphBuilder(object):
+	def __init__(self):
+		self.graph = nx.MultiGraph()
+
+		data_str = open('routes.json').read()
+		data = json.loads(data_str)
+
+		for item in data:
+			route_name = item['name']
+			stops = item['stops']
+			stop_names = [stop['stop']['name'] for stop in stops]
+			if stop_names!=[]:
+				self.graph.add_path(stop_names,route=route_name)
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.post()
+	
+	def post(self):
+		origin = self.get_argument('origin')
+		destination = self.get_argument('destination')
+		all_paths = nx.all_shortest_paths(graph_builder.graph,origin,destination)
+		result = []
+		for path in all_paths:
+			route=[]
+			for k,v in zip(path,path[1:]):
+				edges = graph_builder.graph[k][v]
+				legs = []
+				for edge in edges.values():
+					legs.append(edge['route'])
+				route.append((k,v,legs))
+			result.append(route)
+		self.content_type = 'application/json'
+		self.write(json.dumps(result))
+
+class Application(tornado.web.Application):
+	def __init__(self):
+		handlers = [
+			(r"/", MainHandler)
+		]
+		settings = {
+			"template_path": TEMPLATE_PATH,
+			"static_path": STATIC_PATH,
+		}
+		tornado.web.Application.__init__(self, handlers, **settings)
+
+if __name__ == "__main__":
+	graph_builder = GraphBuilder()
+	application = Application()
+	application.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
